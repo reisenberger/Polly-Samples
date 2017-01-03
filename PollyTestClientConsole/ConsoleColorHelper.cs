c3cd3e90bbc4250c0c029fa4017a02439cf096a1
reisenberger <dmr@ebs.hw.ac.uk>
2017-01-03 16:52:22 +0000

    Move ConsoleColorHelper to PollyTestClientConsole
    Move ConsoleColorHelper to PollyTestClientConsole project

--- PollyTestClientConsole/ConsoleColorHelper.cs
+++ PollyTestClientConsole/ConsoleColorHelper.cs
@@ -3,10 +3,10 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using PollyDemos.OutputHelpers;
 
-namespace PollyDemos.OutputHelpers
+namespace PollyTestClientConsole
 {
-
     public static class ConsoleColorHelper
     {
         public static ConsoleColor ToConsoleColor(this Color color)
